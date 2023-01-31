5733a1e469c20fd6cabb58190fc4619c42301128
alex2022cci <alex2022cci@outlook.fr>
2023-01-31 10:45:53 +0100

    files validation + request

--- system/Request/Request.php
+++ system/Request/Request.php
@@ -2,6 +2,8 @@
 
 namespace system\Request;
 
+use system\Request\Traits\HasFilesValidationRules;
+
 class Request 
 {
     use HasValidationRules, HasFilesValidationRules, HasRunValidations;
@@ -24,32 +26,59 @@ class Request
 
         $rules = $this->rules();
         
-        empty($rules) ? : $this->run($rules);
+        empty($rules) ?  : $this->run($rules);
 
         $this->errorRedirect();
-
+    }
         protected function rules()
         {
             return [];
         }
+
         protected function run($rules)
         {
+            //POUR LES IMG
+            foreach ($rules as $att => $values) {
+                //démonter le tableau dans un autre à cause du ||
+                $ruleArray = explode('|',$values );
+                if(in_array('files', $ruleArray))
+                {
+                    //nous allons détruire les él_ts ds le tableau
+                    unset($ruleArray[array_search('files', $ruleArray)]);
 
+                    //nous allons passer dans une nouvelle fonction la validation
+                    $this->fileValidation($att, $ruleArray);
+
+
+                }
+                elseif(in_array('number', $ruleArray))
+                {
+                    $this->numberValidation($att, $ruleArray);
+                }
+                else 
+                {
+                    //validation normale
+                    $this->normalValidation($att, $ruleArray);
+                }
+            }
         }
 
         protected function file($name)
         {
-
+            return isset($this->files[$name]) ? $this->files[$name] : false ;
         }
 
         protected function postAttributes()
         {
-
+            foreach($_POST as $key => $value)
+            {
+                $this->$key = htmlentities($value);
+                $this->request[$key] = htmlentities($value);
+            }
         }
 
         public function all()
         {
-            
+            return $this->request;
         }
     }
-}
---
+++ system/Request/Traits/HasFilesValidationRules.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace system\Request\Traits;
+
+/**
+ * 
+ */
+trait HasFilesValidationRules
+{
+    //fonction qui permet de refuser les tailles maximum 
+    protected function maxFile($name, $size)
+    {
+       $size = $size * 1024 ;
+       if($this->checkFirstError($name) && $this->checkFileExist($name))
+       {
+        if($this->files[$name]['size'] > $size)
+        {
+            $this->setError($name, "$name size must be lower than ".($size / 1024 )."kb");
+        }
+       }
+    }
+
+      //fonction qui permet de refuser les tailles minimum 
+      protected function minFile($name, $size)
+      {
+        $size = $size * 1024 ;
+        if($this->checkFirstError($name) && $this->checkFileExist($name))
+        {
+         if($this->files[$name]['size'] < $size)
+         {
+             $this->setError($name, "$name size must be greater than ".($size / 1024 )."kb");
+         }
+        }
+      }
+
+      // return type of file
+      private function fileType($name,$typesArray)
+      {
+        if($this->checkFirstError($name) && $this->checkFileExist($name))
+        {
+            $currentTypeFile = explode('/', $this->file[$name]['type'])[1];
+            if(!in_array($currentTypeFile, $typesArray))
+            {
+                $this->setError($name, "$name type must be" . implode(',', $typesArray));
+            }
+        }
+      }
+      //img must be nammed
+      protected function fileRequired($name)
+      {
+        if(!isset($this->files[$name]['name'])|| empty($files[$name]['name']) && $this->checkFirstError($name))
+        {
+            $this->setError($name, '$name is required');
+        }
+      }
+
+      protected function fileValidation($name,$ruleArray)
+      {
+        foreach($ruleArray as $rule)
+        {
+            if($rule === 'required')
+            {
+                $this->fileRequired($name);
+            }
+            elseif( strpos($rule, "mimes: ") === 0 )
+            {
+                $rule = str_replace("mimes:", "", $rule);
+                $rule = explode(',', $rule);
+                $this->fileType($name, $rule);
+            }
+            elseif(strpos($rule, "max:") === 0)
+            {
+                //taille max photo
+                $rule = str_replace("max:", "", $rule);
+                $this->maxFile($name, $rule);
+            }
+            elseif(strpos($rule, "min:") === 0)
+            {
+                //taille min photo
+                $rule = str_replace("min:", "", $rule);
+                $this->minFile($name, $rule);
+            }
+        }
+      }
+}
